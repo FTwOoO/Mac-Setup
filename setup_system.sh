57d8a0e8184c53aa23bee223adbed830d4f6c73b
FTwOoO <FTwOoO@users.noreply.github.com>
2017-05-31 11:02:18 +0800

    Update setup_system.sh

--- setup_system.sh
+++ setup_system.sh
@@ -3,5 +3,24 @@
 sudo softwareupdate -ia --verbose
 xcode-select --install
 
-./osx.sh
-./setup_system.py go python
+### zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+./setup_system.py zsh
+source ~/.zshrc
+
+#./osx.sh
+
+### golang
+./setup_system.py go
+
+# delve debugger
+go get -u github.com/derekparker/delve
+
+# gox
+go get github.com/mitchellh/gox
+cd $GOPATH/src/github.com/mitchellh/gox/main
+go generate
+
+### python
+./setup_system.py python
+pip3 install ansible
